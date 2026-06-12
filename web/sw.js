ab7902da8363cc01cda03276d7b65288e236ec72
Paddy <patrik@charculogic.de>
2026-06-12 09:39:17 +0200

    feat: added automated permission-denied recovery and URL reset trigger for iOS PWAs

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'charculogic-v20260611-167-knowledge-tab';
+const CACHE_NAME = 'charculogic-v20260611-168-knowledge-tab';
 const CACHE_SCHEMA = 'p0-release-hardening-jun2026-ki-wareneingang';
 
 const CRITICAL_ASSETS = [
